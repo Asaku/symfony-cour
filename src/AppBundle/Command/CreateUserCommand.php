6d48b94f3cd631fe986b1ab310afc07b320ea66f
mehdi <kirua-93@hotmail.fr>
2017-01-25 15:03:03 +0100

    add command example

---
+++ src/AppBundle/Command/CreateUserCommand.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace AppBundle\Command;
+
+use Symfony\Bundle\FrameworkBundle\Command\ContainerAwareCommand;
+use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Formatter\OutputFormatterStyle;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+
+class CreateUserCommand extends ContainerAwareCommand
+{
+    /**
+     *
+     */
+    protected function configure()
+    {
+        $this
+            ->setName('dev11:create-user')
+            ->setDescription('Creates new users.')
+            ->setHelp("This command allows you to create users...")
+        ;
+    }
+
+    /**
+     * @param InputInterface $input
+     * @param OutputInterface $output
+     */
+    protected function execute(InputInterface $input, OutputInterface $output)
+    {
+        $this->getContainer()->get('file.manager.image');
+        $style = new OutputFormatterStyle('red', 'yellow', array('bold', 'blink'));
+        $output->getFormatter()->setStyle('fire', $style);
+
+        $output->writeln('<fire>IN FIREEEEEEEE</fire>');
+
+        $output->writeln('<info>haha</info>');
+    }
+}
